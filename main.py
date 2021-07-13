f5986d500a7508c32bbef168db6ca3210fec7511
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-07-13 13:02:38 -0500

    added home apge and finsihed calorie form page and class

--- main.py
+++ main.py
@@ -25,4 +25,30 @@ class CaloriesFormPage(MethodView):
 
         temperature = Temperature(country=calories_form.country.data, city=calories_form.city.data)
 
-        calorie = Calorie(weight=float(calories_form.weight.data, calories_form.height.data, ))
+        calorie = Calorie(
+            weight=float(calories_form.weight.data),
+            height=float(calories_form.height.data),
+            age=float(calories_form.age.data),
+            temperature=temperature)
+        calories = calorie.calculate()
+
+        return render_template('calories_form_page.html',
+                               caloriesform=calories_form,
+                               calories=calories,
+                               result=True)
+
+
+class CaloriesForm(Form):
+    weight = StringField("Weight: ", default=70)
+    height = StringField("Height: ", default=175)
+    age = StringField("Age: ", default=99)
+    country = StringField("Country: ", default='USA')
+    city = StringField('City: ', default='Nashville')
+    button = SubmitField("Calculate")
+
+
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+
+app.add_url_rule('/calories_form', view_func=CaloriesFormPage.as_view('calories_form_page'))
+
+app.run(debug=True)
